160586d1d5ef36140daab076ec7f35658caaf9ac
Edson Siqueira <edson.siqueira@dnx.solutions>
2022-11-04 22:14:04 -0300

    Adding Lambda and other resources for email notification - fixing lint

--- examples/guardduty_alarms.tf
+++ examples/guardduty_alarms.tf
@@ -1,10 +1,10 @@
-module "guardduty" {
-  count  = local.workspace.guardduty.enabled ? 1 : 0
-  source = "git::https://github.com/DNXLabs/terraform-aws-guardduty.git?ref=1.1.1"
+# module "guardduty" {
+#   count  = local.workspace.guardduty.enabled ? 1 : 0
+#   source = "git::https://github.com/DNXLabs/terraform-aws-guardduty.git?ref=1.1.1"
 
-  admin_account_id     = try(local.workspace.guardduty.admin_account_id, "")
-  alarm_slack_severity = local.workspace.guardduty.alarms.enabled ? try(local.workspace.guardduty.alarms.alarm_slack_severity, "HIGH") : ""
-  alarm_slack_webhook  = local.workspace.guardduty.alarms.enabled ? try(local.workspace.guardduty.alarms.slack_endpoint, "") : ""
-  lambda_name          = try(local.workspace.guardduty.lambda_name, "")
-  sns_email_arn        = try(local.workspace.guardduty.sns_email_arn, "")
-}
+#   admin_account_id     = try(local.workspace.guardduty.admin_account_id, "")
+#   alarm_slack_severity = local.workspace.guardduty.alarms.enabled ? try(local.workspace.guardduty.alarms.alarm_slack_severity, "HIGH") : ""
+#   alarm_slack_webhook  = local.workspace.guardduty.alarms.enabled ? try(local.workspace.guardduty.alarms.slack_endpoint, "") : ""
+#   lambda_name          = try(local.workspace.guardduty.lambda_name, "")
+#   sns_email_arn        = try(local.workspace.guardduty.sns_email_arn, "")
+# }
