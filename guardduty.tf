35559b1f1cd28dd7615b058c307a12fed27527b7
Ray Smets <rayjsmets@gmail.com>
2023-02-02 17:40:43 -0800

    variable member detector id

--- guardduty.tf
+++ guardduty.tf
@@ -1,8 +1,10 @@
 resource "aws_guardduty_detector" "member" {
+  count = var.enable_detector ? 1 : 0
   enable = var.enable_detector
 }
 
 resource "aws_guardduty_invite_accepter" "member" {
-  detector_id       = try(aws_guardduty_detector.member.id, var.member_detector_id)
+  # detector_id       = try(aws_guardduty_detector.member.id, var.member_detector_id)
+  detector_id       = var.member_detector_id
   master_account_id = var.admin_account_id
 }
